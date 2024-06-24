34333cb1691f17230b5ecf42742b021c785e4665
Yu-Chang YANG <yang.yc.allium@gmail.com>
2024-06-24 11:00:21 +0800

    perf: speed up linear algebra computation

--- src/JointPoint.jl
+++ src/JointPoint.jl
@@ -8,15 +8,14 @@ using LinearAlgebra
 
 export findjoint, aicc
 
-function rss(xc::AbstractVector{<:AbstractFloat}, yc::AbstractVector{<:AbstractFloat}, 
-			 xj::AbstractVector{<:AbstractFloat}, yj::AbstractVector{<:AbstractFloat})
+function rss(xc, yc, xj, yj)
 	interp = linear_interpolation(xj, yj)
 	return sum((interp.(xc) .- yc) .^ 2)
 end
 
-function xjinner2xjyj(xc, yc, xjinner)
+function xjcore2xjyj(xc, yc, xjcore)
 	T = float(promote_type(eltype(xc), eltype(yc)))
-	xj = [xc[begin]; xjinner; xc[end]]
+	xj = [xc[begin]; xjcore; xc[end]]
 	m = length(xj)
 	dl = zeros(T, m-1)
 	d = zeros(T, m)
@@ -26,45 +25,59 @@ function xjinner2xjyj(xc, yc, xjinner)
 	for j = 1:m-1
 		i = id[j]+1:id[j+1]
 		a, b = xj[j:j+1]
-		d[j:j+1] .+= sum(@.((b-xc[i]) * (xc[i]-a)))
-		du[j] = sum(@.((xc[i]-a) ^ 2))
-		dl[j] = sum(@.((b-xc[i]) ^ 2))
-		right[j] += sum(@.((xc[i]-a) * (b-a) * yc[i]))
-		right[j+1] += sum(@.((b-xc[i]) * (b-a) * yc[i]))
+		bma = b - a
+		bmx = b .- xc[i]
+		xma = xc[i] .- a
+		d[j:j+1] .+= sum(bmx .* xma)
+		du[j] = sum(xma .^ 2)
+		dl[j] = sum(bmx .^ 2)
+		right[j] += bma * sum(xma .* yc[i])
+		right[j+1] += bma * sum(bmx .* yc[i])
 	end
 	left = Tridiagonal(dl, d, du)
-	yj = pinv(left) * right
+	yj = try
+		left \ right
+	catch
+		pinv(left) * right
+	end
 	return xj, yj
 end
 
-function findjoint(xc::AbstractVector{<:AbstractFloat}, 
-				   yc::AbstractVector{<:AbstractFloat}, k::Integer; kwargs...)
-	@assert issorted(xc, lt=<=)
+function findjoint(xc::AbstractVector{T}, 
+				   yc::AbstractVector{T}, k::Integer; kwargs...) where {T<:AbstractFloat}
+	issorted(xc, lt=<=) || throw(ArgumentError("`xc` must be strictly increasing"))
 	n = length(xc)
-	@assert n == length(yc)
-	function f(xjinner)
-		xj, yj = xjinner2xjyj(xc, yc, sort(xjinner))
+	n == length(yc) || throw(ArgumentError("`xc` and `yc` must have the same length"))
+	function f(xjcore)
+		xj, yj = xjcore2xjyj(xc, yc, sort(xjcore))
 		return rss(xc, yc, xj, yj)
 	end
-	xjinner = if k > 0
+	xjcore = if k > 0
 		searchrange = fill((xc[begin], xc[end]), k)
-		res = bboptimize(f; SearchRange=searchrange, MaxStep=300000, MaxTime=10, 
-			MinDeltaFitnessTolerance=1e-20, kwargs...)
+		res = bboptimize(f; SearchRange=searchrange, TraceMode=:silent, MaxTime=5*k, kwargs...)
+		startswith(BlackBoxOptim.stop_reason(res), "Max time") && 
+			@warn("optimization process may not have converged")
 		best_candidate(res)
 	else
-		eltype(xc)[]
+		T[]
 	end
-	return xjinner2xjyj(xc, yc, sort(xjinner))
+	return xjcore2xjyj(xc, yc, sort(xjcore))
 end
-findjoint(xc::AbstractVector{<:Real}, yc::AbstractVector{<:Real}, k::Integer) = 
-	findjoint(float(xc), float(yc), k)
+findjoint(xc::AbstractVector{<:AbstractFloat}, 
+		  yc::AbstractVector{<:AbstractFloat}, k::Integer; kwargs...) = 
+	findjoint(promote(xc, yc)..., k; kwargs...)
+findjoint(xc::AbstractVector{<:Real}, yc::AbstractVector{<:Real}, k::Integer; kwargs...) = 
+	findjoint(float(xc), float(yc), k; kwargs...)
 
-function aicc(xc::AbstractVector{<:AbstractFloat}, yc::AbstractVector{<:AbstractFloat}, 
-			  xj::AbstractVector{<:AbstractFloat}, yj::AbstractVector{<:AbstractFloat})
+function aicc(xc::AbstractVector{T}, yc::AbstractVector{T}, 
+			  xj::AbstractVector{T}, yj::AbstractVector{T}) where {T<:AbstractFloat}
 	n = length(xc)
 	karg = length(xj) * 2 - 2
 	return 2 * karg * n / (n - karg - 1) + n * log(rss(xc, yc, xj, yj) / n)
 end
+aicc(xc::AbstractVector{<:AbstractFloat}, yc::AbstractVector{<:AbstractFloat}, 
+	 xj::AbstractVector{<:AbstractFloat}, yj::AbstractVector{<:AbstractFloat}) = 
+	 	aicc(promote(xc, yc, xj, yj)...)
 aicc(xc::AbstractVector{<:Real}, yc::AbstractVector{<:Real}, 
 	 xj::AbstractVector{<:Real}, yj::AbstractVector{<:Real}) = 
 		aicc(float(xc), float(yc), float(xj), float(yj))
